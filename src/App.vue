03f6494ef1e29587729e80c11fbbb505d513ebb1
brellin <cekakid@gmail.com>
2020-05-06 21:00:01 -0500

    add vue dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <header>
+      <h1>Custom Tracker</h1>
+    </header>
   </div>
 </template>
+
+<style lang="scss">
+html {
+  font-size: 62.5%;
+}
+body {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  margin: 0;
+  background: whitesmoke;
+}
+div#app {
+  header {
+    @include flex(column, flex-start, center);
+    background: linear-gradient(#f5f5f5, #f1bdbd, #f5f5f5);
+    width: 100%;
+
+    h1 {
+      font-size: 5rem;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+import Vuex, { mapActions } from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Router from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+Vue.use(Router)
+Vue.use(VueAxios, axios)
+
+const store = new Vuex.Store({
+  state: {
+    loggedIn: false
+  },
+  actions: {
+    login(store, user) {
+      if (user) store.state.loggedIn = true
+      else store.state.loggedIn = false
+    }
+  },
+  mutations: {
+    ...mapActions(['login'])
+  }
+})
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
